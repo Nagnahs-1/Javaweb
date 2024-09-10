83afc4bde6984998ebeaf846992db8e26b4111d2
zyf <2695274770@qq.com>
2024-09-10 17:35:49 +0800

    class02

--- class01-serverlet/src/main/java/com/top/class01serverlet/ServletDemo2.java
+++ class01-serverlet/src/main/java/com/top/class01serverlet/ServletDemo2.java
@@ -15,6 +15,8 @@ public class ServletDemo2 implements Servlet {
      * @throws ServletException
      */
     @Override
+//    Servlet 接口的 init 方法的实现。当servlet被Web容器加载并准备服务时，
+//    这个方法会被调用一次。在这个实现中，它简单地打印了一条消息到控制台，表明servlet已经被初始化
     public void init(ServletConfig servletConfig) throws ServletException {
         System.out.println("ServletDemo2 初始化");
 
@@ -26,6 +28,7 @@ public class ServletDemo2 implements Servlet {
      * @return
      */
     @Override
+//    这个方法应该返回封装了servlet初始化参数的 ServletConfig 对象。
     public ServletConfig getServletConfig() {
         return null;
     }
@@ -39,6 +42,8 @@ public class ServletDemo2 implements Servlet {
      * @throws IOException
      */
     @Override
+//    service 方法是servlet的核心，负责处理所有进入的请求。
+//    在这个实现中，它使用 getWriter 方法获取一个 PrintWriter 对象，并写入“ServletDemo2 Service”字符串作为响应内容。
     public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
         servletResponse.getWriter().write("ServletDemo2 Service");
 
@@ -49,6 +54,7 @@ public class ServletDemo2 implements Servlet {
      * @return
      */
     @Override
+//返回一个描述servlet的字符串
     public String getServletInfo() {
         return "";
     }
@@ -57,6 +63,7 @@ public class ServletDemo2 implements Servlet {
      * 销毁方法
      */
     @Override
+//    destroy 方法在servlet不再需要，并且服务器决定卸载它时调用。
     public void destroy() {
         System.out.println("ServletDemo2 销毁");
     }
--- class01-serverlet/src/main/java/com/top/class01serverlet/ServletDemo3.java
+++ class01-serverlet/src/main/java/com/top/class01serverlet/ServletDemo3.java
@@ -6,8 +6,11 @@ import jakarta.servlet.annotation.WebServlet;
 
 import java.io.IOException;
 
-
-@WebServlet(name = "servletDemo3", value = "/servlet3",loadOnStartup = 2)
+//这个 @WebServlet 注解用于将 ServletDemo3 类声明为一个 Servlet，并指定了它的几个关键属性：
+@WebServlet(name = "servletDemo3", //指定了 Servlet 的名称为 servletDemo3
+        value = "/servlet3",//指定了访问该 Servlet 的 URL 模式为 /servlet3。客户端可以通过这个 URL 来请求 Servlet。
+        loadOnStartup = 2//指定了 Servlet 的加载顺序。
+)
 public class ServletDemo3 implements Servlet {
 
     /**
@@ -18,6 +21,7 @@ public class ServletDemo3 implements Servlet {
      * @throws ServletException
      */
     @Override
+    //这是 Servlet 接口的 init 方法的实现。当 Servlet 被 Web 容器加载并准备服务时，这个方法会被调用一次
     public void init(ServletConfig servletConfig) throws ServletException {
         System.out.println("ServletDemo3 初始化");
 
@@ -27,6 +31,7 @@ public class ServletDemo3 implements Servlet {
      * 获取Servlet 配置
      */
     @Override
+    //这个方法应该返回封装了 Servlet 初始化参数的 ServletConfig 对象。
     public ServletConfig getServletConfig() {
         return null;
     }
@@ -40,6 +45,7 @@ public class ServletDemo3 implements Servlet {
      * @throws IOException
      */
     @Override
+    //service 方法是 Servlet 的核心，负责处理所有进入的请求。
     public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
         servletResponse.getWriter().write("ServletDemo3 Service");
 
@@ -49,6 +55,7 @@ public class ServletDemo3 implements Servlet {
      * 获取 Servlet信息
      */
     @Override
+    //这个方法应该返回一个描述 Servlet 的字符串
     public String getServletInfo() {
         return "";
     }
@@ -57,6 +64,7 @@ public class ServletDemo3 implements Servlet {
      * 销毁方法
      */
     @Override
+    //destroy 方法在 Servlet 不再需要，并且服务器决定卸载它时调用
     public void destroy() {
         System.out.println("ServletDemo3 销毁");
     }
